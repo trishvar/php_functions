c074aa8601d91dff6236a5cf1a0320b6f624dfb9
trishvar <trishvar@outlook.com>
2024-01-18 07:50:40 +0530

    add new file and update readme file

---
+++ class.func.php
@@ -0,0 +1,26 @@
+<?php
+        class triFunc{
+
+            public function pagination(){
+                echo "
+                <ul>
+                    <li>
+                        <a href='?page=1'>Prev</a>
+                    </li>
+                    <li>
+                        <a href='?page=1'>1</a>
+                    </li>
+                    <li>
+                        <a href='?page=2'>2</a>
+                    </li>
+                    <li>
+                        <a href='?page=3'>3</a>
+                    </li>
+                    <li>
+                        <a href='?page=3'>Next</a>
+                    </li>
+                </ul>
+                ";
+            }
+        }
+?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+        require "class.func.php";
+?>
